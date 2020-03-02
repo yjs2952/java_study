cf544a63c190834576b310808cbf2713f7690824
yjs <yjs2952@naver.com>
2020-03-02 22:51:21 +0900

    tdd 3장 예제 추가

--- test_driven_development/src/test/java/ch03/ExpiryDateCalculatorTest.java
+++ test_driven_development/src/test/java/ch03/ExpiryDateCalculatorTest.java
@@ -93,6 +93,22 @@ public class ExpiryDateCalculatorTest {
                         .payAmount(20_000)
                         .build()
                 , LocalDate.of(2019, 4, 30));
+
+        assertExpiryDate(
+                PayData.builder()
+                        .firstBillingDate(LocalDate.of(2019, 1, 31))
+                        .billingDate(LocalDate.of(2019, 2, 28))
+                        .payAmount(40_000)
+                        .build()
+                , LocalDate.of(2019, 6, 30));
+
+        assertExpiryDate(
+                PayData.builder()
+                        .firstBillingDate(LocalDate.of(2019, 3, 31))
+                        .billingDate(LocalDate.of(2019, 4, 30))
+                        .payAmount(30_000)
+                        .build()
+                , LocalDate.of(2019, 7, 31));
     }
 
     private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
