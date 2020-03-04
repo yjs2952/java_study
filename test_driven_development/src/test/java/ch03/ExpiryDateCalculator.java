67f5833c084a3c033cb0f4c5c11440015cc94482
yjs <yjs2952@naver.com>
2020-03-04 22:18:40 +0900

    tdd 3장 코드 가독성 개선

--- test_driven_development/src/test/java/ch03/ExpiryDateCalculator.java
+++ test_driven_development/src/test/java/ch03/ExpiryDateCalculator.java
@@ -9,20 +9,34 @@ public class ExpiryDateCalculator {
         int addedMonths = payData.getPayAmount() / 10_000;
 
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+            return expiryDateUsingBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
+        }
+    }
 
+    private LocalDate expiryDateUsingBillingDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+
+        if (isSameDayOfMonth(candidateExp, payData.getFirstBillingDate())) {
+
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
             final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
 
-            if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
-
-                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-
-                if (dayLenOfCandiMon < dayOfFirstBilling) {
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
-                }
-                return candidateExp.withDayOfMonth(dayOfFirstBilling);
+            if (dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
             }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
         }
-        return payData.getBillingDate().plusMonths(addedMonths);
+    }
+
+    private int lastDayOfMonth(LocalDate date) {
+        return YearMonth.from(date).lengthOfMonth();
+    }
+
+    boolean isSameDayOfMonth(LocalDate date1, LocalDate date2) {
+        return date1.getDayOfMonth() != date2.getDayOfMonth();
     }
 }
