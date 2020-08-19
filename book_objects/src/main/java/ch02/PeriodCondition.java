dc6aa3efa410a670080becbc09f862aee9b9b1ef
yjs2952 <yjs2952@naver.com>
2020-08-19 14:56:23 +0900

    영화 예매 시스템 설계

---
+++ book_objects/src/main/java/ch02/PeriodCondition.java
@@ -0,0 +1,24 @@
+package ch02;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class PeriodCondition implements DiscountCondition {
+
+    private DayOfWeek dayOfWeek;
+    private LocalDate startTime;
+    private LocalDate endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalDate startTime, LocalDate endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.getStartTime().getDayOfWeek().equals(dayOfWeek) &&
+                startTime.compareTo(screening.getStartTime().toLocalDate()) <= 0 &&
+                endTime.compareTo(screening.getStartTime().toLocalDate()) >= 0;
+    }
+}
